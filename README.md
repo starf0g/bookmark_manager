f9377cbed071052832c7838d58abd75640c03f1a
Alex Boyling <alexboyling@gmail.com>
2021-10-12 15:26:18 +0100

    Setup test database and automated use in testing

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 4) Run the query saved in the file `01_create_bookmarks_table.sql` located in db/migrations
 
 You can run the file using the `\i` command in psql
+
+## Test Database setup instructions
+1) Connect to `psql`
+2) Create the database using the `psql` command `CREATE DATABASE "bookmark_manager_test";`
+3) Connect to the database using the `psql` command `\c bookmark_manager_test;`
+4) Run the query saved in the file `01_create_bookmarks_table.sql` located in db/migrations UPDATE THIS LATER
+
+You can run the file using the `\i` command in psql
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,12 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'return a list of all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      # Test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include "http://www.makersacademy.com"
